c0ff265511b64a43883962512513084393d7bef3
JFAR-3 <201907331@alu.comillas.edu>
2025-03-14 10:54:12 +0100

    Añado archivos

---
+++ Code snippets/commands.sh
@@ -0,0 +1,29 @@
+git clone git@github.com:hellofresh/klepto.git
+
+git add .
+
+git branch my_new_branch
+
+git checkout my_new_branch
+
+git commit -m "descriptive message"
+
+git diff
+
+git log
+
+git blame file_name.txt
+
+git stash && git checkout different_branch && git stash pop
+
+git checkout main && git merge my_new_branch
+
+git rebase main
+
+git fetch --all
+
+git pull
+
+git push
+
+
